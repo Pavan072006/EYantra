cf69ea6d33d89acf2eeb0dd2ad8ac99522995558
Pavan072006 <ee24b048@smail.iitm.ac.in>
2026-08-16 02:57:32 +0530

    Update riscv_cpu.v

--- RISC-V CPU (Single-Cycle)/riscv_cpu.v
+++ RISC-V CPU (Single-Cycle)/riscv_cpu.v
@@ -11,17 +11,18 @@ module riscv_cpu (
     output [31:0] Result
 );
 
-wire        ALUSrc, RegWrite, Jump, Jalr, Zero , ALUR31;
+wire        ALUSrc, RegWrite, Jump, Jalr, EQ, LTS, LTU;
+wire        PCSrc;
 wire [1:0]  ResultSrc, ImmSrc;
 wire [3:0]  ALUControl;
 
-controller  c   (Instr[6:0], Instr[14:12], Instr[30], Zero, ALUR31,
+controller  c   (Instr[6:0], Instr[14:12], Instr[30], EQ, LTS, LTU,
                 ResultSrc, MemWrite, PCSrc, ALUSrc, RegWrite, Jump, Jalr,
                 ImmSrc, ALUControl);
 
 datapath    dp  (clk, reset, ResultSrc, PCSrc,
                 ALUSrc, RegWrite, ImmSrc, ALUControl, Jalr,
-                Zero, ALUR31,PC, Instr, Mem_WrAddr, Mem_WrData, ReadData, Result);
+                EQ, LTS, LTU, PC, Instr, Mem_WrAddr, Mem_WrData, ReadData, Result);
 
 endmodule
 
